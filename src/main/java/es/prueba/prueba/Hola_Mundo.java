a6ae898e29fa4ebfc36e5b1ddd2173ccf61d9d01
Voltex66 <rubencanalespro@gmail.com>
2024-12-16 10:57:07 +0100

    Hacemos de nuevo la prueba

---
+++ src/main/java/es/prueba/prueba/Hola_Mundo.java
@@ -0,0 +1,10 @@
+package es.prueba.prueba;
+
+public class Hola_Mundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hola mundo");
+	}
+
+}
